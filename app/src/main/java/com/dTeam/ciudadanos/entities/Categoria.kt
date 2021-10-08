74a612f9171d799a84ff7f7ada10887f5ec29e08
fbsteinberg <fedesteinberg@gmail.com>
2021-10-08 16:24:08 -0300

    Agregada la lista de subcategorias a la clase Categoria

--- app/src/main/java/com/dTeam/ciudadanos/entities/Categoria.kt
+++ app/src/main/java/com/dTeam/ciudadanos/entities/Categoria.kt
@@ -1,8 +1,11 @@
 package com.dTeam.ciudadanos.entities
 
+import com.example.gestiondereclamos.entities.Subcategoria
+
 class Categoria(
     var title : String,
     var description: String,
+    var subcategorias : MutableList<Subcategoria>
     // var tipoReclamo : MutableList<TipoReclamo>
 ) {
 
