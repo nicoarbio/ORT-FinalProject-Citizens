48f274c351838838f5552357216885ab7f4bb084
fbsteinberg <fedesteinberg@gmail.com>
2021-10-14 00:15:25 -0300

    deshacer commit

--- app/src/main/java/com/dTeam/ciudadanos/viewmodels/ReclamoListViewModel.kt
+++ app/src/main/java/com/dTeam/ciudadanos/viewmodels/ReclamoListViewModel.kt
@@ -1,7 +1,42 @@
 package com.dTeam.ciudadanos.viewmodels
 
-import androidx.lifecycle.ViewModel
+import android.util.Log
+import androidx.lifecycle.*
+import androidx.recyclerview.widget.RecyclerView
+import com.dTeam.ciudadanos.entities.Reclamo
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.firestore.ktx.toObject
+import com.google.firebase.ktx.Firebase
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.tasks.await
+import java.lang.Exception
 
 class ReclamoListViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+
+    val db = Firebase.firestore
+    private var reclamoList : MutableList<Reclamo> = mutableListOf()
+    val listadoReclamos = MutableLiveData<MutableList<Reclamo>>()
+
+     fun getReclamos() {
+
+         viewModelScope.launch {
+             reclamoList.clear()
+             try {
+                 val reclamos = db.collection("reclamos")
+                     .whereEqualTo("usuario", "fperchuk@hotmail.com") //TODO: Acá poner el mail del usuario logueado!
+                     .get()
+                     .await()
+                 if (reclamos != null) {
+                     for (reclamo in reclamos) {
+                         reclamoList.add(reclamo.toObject())
+                     }
+                     listadoReclamos.value =  reclamoList
+                 }
+             }catch (e: Exception){
+                 Log.w("Test", "Error al obtener documentos: ", e)
+             }
+
+         }
+
+    }
 }
