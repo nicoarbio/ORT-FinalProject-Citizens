71cbeaa3a9cbc3e999f489ef5d369e543afa5e7c
fperchuk <fperchuk@hotmail.com>
2021-11-12 19:46:00 -0300

    Botón cerrar sesión

--- app/src/main/java/com/dTeam/ciudadanos/fragments/PerfilUsuario.kt
+++ app/src/main/java/com/dTeam/ciudadanos/fragments/PerfilUsuario.kt
@@ -6,9 +6,11 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.lifecycle.Observer
+import androidx.navigation.findNavController
 import com.bumptech.glide.Glide
 import com.dTeam.ciudadanos.R
 import com.dTeam.ciudadanos.adapters.ImgReclamoAdapter
@@ -22,7 +24,7 @@ class PerfilUsuario : Fragment() {
     }
     lateinit var v: View
     private lateinit var usuarioViewModel: UsuarioViewModel
-
+    private lateinit var btnCerrarSesion : Button
     private lateinit var txtNombreApellido : TextView
     private lateinit var txtEmail : TextView
     private lateinit var txtTelefono : TextView
@@ -40,6 +42,14 @@ class PerfilUsuario : Fragment() {
         txtDni = v.findViewById(R.id.lblDni)
         txtDireccion = v.findViewById(R.id.lblDireccionPerfil)
         txtTelefono = v.findViewById(R.id.lblTelefonoPerfil)
+        btnCerrarSesion = v.findViewById(R.id.btnCerrarSesion)
+
+        btnCerrarSesion.setOnClickListener{
+            usuarioViewModel.cerrarSesion()
+            val action = PerfilUsuarioDirections.actionPerfilUsuarioToLogIn()
+            v.findNavController().navigate(action)
+        }
+
         return v
     }
 
